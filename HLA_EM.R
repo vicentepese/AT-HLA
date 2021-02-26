89bd7ed30dc1fc85e63a85ae20f1c9e556875ade
vicentepese <sempereperisvicente@gmail.com>
2021-02-26 08:34:21 -0800

    haplo count to function

--- HLA_EM.R
+++ HLA_EM.R
@@ -1,9 +1,9 @@
 ## ---------------------------
 ##
-## Name: HLA_haplotype.R
+## Name: HLA_EM.R
 ##
-## Desciption: Given a dataset of HLA calls and a list of alleles,
-##              it computes an haplotype analysis.
+## Desciption: Given a dataset of HLA calls it performs 
+##              expectation maximization to find haplotypes.
 ##
 ## Author: Vicente Peris Sempere
 ##
@@ -69,44 +69,60 @@ if (!settings$ethnicity %>% is_empty()){
     filter(sample.id %in% ethnicity.df.filt$sample.id)
 }
 
+################# COMPUTE EM ###################
+
+haplo_EM = function(settings, HLA.df){
+  
+  # Parse loci of interest and append phenotypes to HLA calls
+  loci <- settings$EM_haplo %>% unlist()
+  loci.IDs <- paste0(rep(loci,each = 2), rep(c(".1", ".2"), length(loci)))
+  HLA.df <- HLA.df %>% merge(covars.df[,c("sample.id", "pheno")])
+  
+  # Filter out HLA calls wiht low imputation probabilities 
+  probs.loci.id <- paste0(rep("prob.", length(loci)), loci)
+  probs.df.filt <- probs.df %>% filter_at(.vars = probs.loci.id, all_vars(.>prob_thr))
+  HLA.df <- HLA.df %>% filter(sample.id %in% probs.df.filt$sample.id)
+  
+  # Compute expectation maximization
+  haplo.em.df <- haplo.em(HLA.df[,loci.IDs], locus.label = loci)
+  
+  # Get haplotypes and haplotype indexes
+  haplotypes <- haplo.em.df$haplotype
+  subj.idx <- haplo.em.df$subj.id
+  hap1.code <- haplo.em.df$hap1code
+  hap2.code <- haplo.em.df$hap2code
+  
+  # Create dataframe with haplotypes 
+  haplo.df <- cbind(data.frame(sample.id = HLA.df$sample.id[haplo.em.df$subj.id]), haplotypes[hap1.code,])
+  haplo.df2 <- cbind(data.frame(sample.id = HLA.df$sample.id[haplo.em.df$subj.id]), haplotypes[hap2.code,])
+  haplo.df <- rbind(haplo.df, haplo.df2)
+  
+  # Remove duplicates (include only carriers / extended haplotype)
+  haplo.df <- haplo.df[!duplicated(haplo.df),]
+  
+  # Add phenotype
+  haplo.df <- merge(haplo.df, covars.df[,c("sample.id", "pheno")], by = "sample.id")
+  
+  # Count haplotypes from cases and controls
+  haplo.count.cases <- haplo.df %>% filter(pheno ==1) %>% count(loci)
+  colnames(haplo.count.cases)[ncol(haplo.count.cases)] <- "Count.Cases"
+  haplo.count.controls <- haplo.df %>% filter(pheno == 0) %>% count(loci)
+  colnames(haplo.count.controls)[ncol(haplo.count.controls)] <- "Count.Controls"
+  haplo.count <- merge(haplo.count.cases, haplo.count.controls, by = loci, all = TRUE)
+  
+  # Replace NAs with 0s
+  haplo.count[is.na(haplo.count)] <- 0
+  
+  # Compute frequencies 
+  Ncases <- HLA.df$pheno %>% table() %>% .["1"]; Ncontrols <- HLA.df$pheno %>% table() %>% .["0"]
+  haplo.count <- haplo.count %>% add_column(Freq.Cases = haplo.count$Count.Cases / Ncases *100, .after = "Count.Cases")
+  haplo.count <- haplo.count %>% add_column(Freq.Controls = haplo.count$Count.Controls /Ncontrols *100, .after = "Count.Controls")
+  
+  
+}
+
 ################## COUNT HAPLOTYPES ################## 
 
-# Parse loci of interest 
-loci <- c("DQA1","DQB1","DRB1", "DRB4")
-loci.IDs <- paste0(rep(loci,each = 2), rep(c(".1", ".2"), length(loci)))
-HLA.df <- HLA.df %>% merge(covars.df[,c("sample.id", "pheno")])
-
-# Filter out HLA calls wiht low imputation probabilities 
-probs.loci.id <- paste0(rep("prob.", length(loci)), loci)
-probs.df.filt <- probs.df %>% filter_at(.vars = probs.loci.id, all_vars(.>prob_thr))
-HLA.df <- HLA.df %>% filter(sample.id %in% probs.df.filt$sample.id)
-
-# Compute expectation maximization
-haplo.em.df <- haplo.em(HLA.df[,loci.IDs], locus.label = loci)
-
-# Get haplotypes and haplotype indexes
-haplotypes <- haplo.em.df$haplotype
-subj.idx <- haplo.em.df$subj.id
-hap1.code <- haplo.em.df$hap1code
-hap2.code <- haplo.em.df$hap2code
-
-# Create dataframe with haplotypes 
-haplo.df <- cbind(data.frame(sample.id = HLA.df$sample.id[haplo.em.df$subj.id]), haplotypes[hap1.code,])
-haplo.df2 <- cbind(data.frame(sample.id = HLA.df$sample.id[haplo.em.df$subj.id]), haplotypes[hap2.code,])
-haplo.df <- rbind(haplo.df, haplo.df2)
-
-# Remove duplicates (include only carriers)
-haplo.df <- haplo.df[!duplicated(haplo.df),]
-
-# Add pheno 
-haplo.df <- merge(haplo.df, covars.df[,c("sample.id", "pheno")], by = "sample.id")
-
-# Count haplotypes 
-haplo.count.cases <- haplo.df %>% filter(pheno ==1) %>% count(loci)
-colnames(haplo.count.cases)[ncol(haplo.count.cases)] <- "Count.Cases"
-haplo.count.controls <- haplo.df %>% filter(pheno == 0) %>% count(loci)
-colnames(haplo.count.controls)[ncol(haplo.count.controls)] <- "Count.Controls"
-haplo.count <- merge(haplo.count.cases, haplo.count.controls, by = loci, all = TRUE)
 
 # Get reference 
 HLA.df.ref <- HLA.df %>% filter(DRB1.1 != "07:01" & DRB1.2 != "07:01")
@@ -114,10 +130,3 @@ HLA.df.ref <- HLA.df %>% filter(DRB1.1 != "07:01" & DRB1.2 != "07:01")
 # Compute Chi-square 
 
 
-
-###############3 REFERENCE ###############
-
-tst <- HLA.df %>% filter( (DQA1.1 == "02:01" | DQA1.2 == "02:01") &
-                            (DQB1.1 == "02:02" | DQB1.2 == "02:02") &
-                            (DRB1.1 == "07:01" | DRB1.2 == "07:01") &
-                            (DRB4.1 == "01:01" | DRB4.2 == "01:01") )
