ccdb81f2b96ec64fdc903e9ef0bfadb25f2e8b71
vicentepese <sempereperisvicente@gmail.com>
2021-03-02 15:08:13 -0800

    init + funct

---
+++ HLA_zygosity.R
@@ -0,0 +1,184 @@
+## ---------------------------
+##
+## Name: HLA_zygosity.R
+##
+## Desciption: Given a dataset of HLA calls, computes a  
+##    zygosity study of each allele.
+##
+## Author: Vicente Peris Sempere
+##
+## Year: 2021
+##
+## Copyright (c) Vicente Peris Sempere, 2021
+## Email: vipese@stanford.edu
+##
+## ---------------------------
+
+# Import libraries
+library(jsonlite)
+library(tidyverse)
+library(readr)
+library(data.table)
+library(xlsx)
+library(plyr)
+
+########### INITIALIZATION ############
+
+# Set working directory
+setwd("~/Documents/HLA_association_pipeline")
+
+# Import settings
+settings <- jsonlite::read_json("settings.json")
+
+# Create comand
+`%notin%` <- Negate(`%in%`)
+
+# Import HLA calls, covariates 
+HLA.df <- read.csv(settings$file$HLA_Data)
+covars.df <- read.csv(settings$file$covars)
+probs.df <- read.csv(settings$file$probs)
+
+# Read options
+prob_thr <- settings$prob_thr
+freq_thr <- settings$freq_thr*100
+
+# Parse HLA calls for which there is a phenotype 
+HLA.df <- HLA.df %>% filter(sample.id %in% covars.df$sample.id)
+
+# Parse HLA calls based on ethnicity, if provided
+if (!settings$ethnicity %>% is_empty()){
+  
+  # Parse IDs in ethnicity/ies
+  ethnicity.df <- read.csv(settings$file$ethnicity)
+  ethnicity.df.filt <- ethnicity.df %>% filter(Population %in% settings$ethnicity %>% unlist())
+  HLA.df <- HLA.df %>% 
+    filter(sample.id %in% ethnicity.df.filt$sample.id)
+}
+
+# Delete files to allow output to be written
+file.names <- list.files(settings$Output$Zygosity, full.names = TRUE)
+file.names <- file.names[grepl(file.names, pattern = "HLA_Zygosity.xlsx")]
+file.remove(file.names)
+
+############ COMPUTE ALLELE/CARRIER COUNT/FREQUENCIES ############
+
+computeACFREQ = function(data, locus, Dx){
+  # Compute heterozigous, homozigous, or absence count. 
+  # Compute allele frequency, count and total.
+  # Compute carrier frequency, count and total.
+  
+  # Parse alleles
+  A1 <- locus %>% paste0('.1'); A2 <- locus %>% paste0('.2')
+  alleles <- list(data[, A1], 
+                  data[, A2]) %>% unlist()
+
+  # Carrier frequency
+  carriers <- data[,c(A1, A2)]
+  carriers.levels <- list(data[, A1], 
+                          data[, A2]) %>% unlist() %>% levels()
+  carriers.unique <- apply(carriers, 1, function(x) unique(x)) %>% unlist() %>% as.factor()
+  carriers.count <- table (carriers.unique); carriers.count[c(carriers.levels %>% setdiff(carriers.count %>% names()))] <- 0
+  carriers.freq <- carriers.count /nrow(data) * 100
+  carrier.df <- data.frame(allele = carriers.count %>% names(), 
+                           carrierCount = carriers.count %>% as.vector(),
+                           carrierFreq = carriers.freq %>% as.vector(),
+                           carrierTotal = nrow(data))
+  
+  # Heterozigous, homozigous, and absence count
+  A0 <- c(); A1 <- c(); A2 <- c();
+  for (A in levels(as.factor(alleles))){
+    HH.data <- carriers[which(carriers[,1]==as.character(A) | carriers[,2]==as.character(A)),]
+    HH.count <- HH.data %>% apply(1, function(x) x %>% unlist() %>% unique() %>% length()) %>% unlist() %>% table()
+    A0 <- c(A0, nrow(data) - nrow(HH.data)); A1 <- c(A1,HH.count['2'] %>% unname()); A2 <- c(A2,HH.count['1'] %>% unname())
+  }
+  A1[is.na(A1)] <- 0; A2[is.na(A2)] <- 0; HH.data <- data.frame(allele = levels(as.factor(alleles)), A0 = A0, A1 = A1, A2 = A2)
+  
+  # Compute frequencies 
+  HH.data$FreqHetero <- HH.data$A1 / (HH.data$A0 + HH.data$A1 + HH.data$A2) *100
+  HH.data$FreqHomo <- HH.data$A2/ (HH.data$A0 + HH.data$A1 + HH.data$A2)*100
+  
+  # Merge 
+
+  switch (Dx,
+          'case' = {
+            colnames(HH.data) <- c('allele', paste0(colnames(HH.data) %>% tail(n=-1), rep("Case", length(colnames(HH.data))-1)))
+            return(HH.data)
+          },
+          'control' = {
+            colnames(HH.data) <-c('allele', paste0(colnames(HH.data) %>% tail(n=-1), rep("Control", length(colnames(HH.data))-1)))
+            return(HH.data)
+          }
+  )
+}
+
+
+############ COMPUTE CHI SQUARE ##############
+
+computeChi2 = function(ACFREQ.df){
+  
+  # Compute Chi2 and OR for each allele
+  ACFREQ.df$Chi2.Hetero.pval <- NA; ACFREQ.df$OR.Hetero <- NA; ACFREQ.df$Chi2.Homo.pval <- NA; ACFREQ.df$OR.Homo <- NA
+  for (i in 1:nrow(ACFREQ.df)){
+    
+    # Heterozygous
+    cont.table.hetero <- matrix(c(ACFREQ.df$A1Case[i], ACFREQ.df$A1Control[i],
+                                  ACFREQ.df$A0Case[i], ACFREQ.df$A0Control[i]), nrow = 2)
+    ACFREQ.df$Chi2.Hetero.pval[i] <-  chisq.test(cont.table.hetero) %>%.["p.value"] %>% unlist() %>% unname()
+    ACFREQ.df$OR.Hetero[i] <- (cont.table.hetero[1]*cont.table.hetero[4])/(cont.table.hetero[2]*cont.table.hetero[3])
+    
+    # Homozygous
+    cont.table.homo <- matrix(c(ACFREQ.df$A2Case[i], ACFREQ.df$A2Control[i],
+                                ACFREQ.df$A0Case[i], ACFREQ.df$A0Control[i]), nrow = 2)
+    ACFREQ.df$Chi2.Homo.pval[i] <- chisq.test(cont.table.homo) %>%.["p.value"] %>% unlist() %>% unname()
+    ACFREQ.df$OR.Homo[i] <- (cont.table.homo[1]*cont.table.homo[4])/(cont.table.homo[2]*cont.table.homo[3])
+  }
+  
+  # Return dataframe with ORs and Chi2s p-values
+  return(ACFREQ.df)
+  
+}
+
+
+############# MAIN LOOP ##############
+
+# Get cases and controls and separate datasets
+cases.ids <- covars.df$sample.id[which(covars.df$pheno ==2)]
+controls.ids <- covars.df$sample.id[which(covars.df$pheno ==1)]
+data.cases <- HLA.df %>% filter(sample.id %in% cases.ids)
+data.controls <- HLA.df %>% filter(sample.id %in% controls.ids)
+
+# Initialize multiple-test correction 
+pvals <- c(); l1group <- c(); l2group <- c(); l2locus <- c()
+
+# For each locus 
+loci <- c("A","B","C","DPB1", "DQA1", "DQB1", "DRB1", "DRB3", "DRB4", "DRB5")
+
+idx <- 1
+for (locus in loci){
+  
+  # Verbose
+  print(paste0("Current locus: ", locus))
+  
+  # Filter out subjects with imputation probability threshold
+  probs.df_filt <- probs.df %>% filter(get(paste0("prob.", locus)) > prob_thr)
+  data.cases.filt <- data.cases %>% filter(sample.id %in% probs.df_filt$sample.id)
+  data.controls.filt <- data.controls %>% filter(sample.id %in% probs.df_filt$sample.id)
+  
+  # Compute allele and carrier counts and frequencies
+  ACFREQ.cases <- computeACFREQ(data.cases.filt, locus, 'case');
+  ACFREQ.controls <- computeACFREQ(data.controls.filt, locus, 'control');
+
+  # Merge and clean 
+  ACFREQ.df <- merge(ACFREQ.cases[,!names(ACFREQ.cases) %in% c('A0','A1','A2')], ACFREQ.controls, by = 'allele', all = TRUE) 
+  ACFREQ.df[is.na(ACFREQ.df)] <- 0
+  ACFREQ.df <- ACFREQ.df %>% filter(allele %notin% c('','00:00'))
+  
+  # Compute Chi2
+  ACFREQ.df <- computeChi2(ACFREQ.df)
+  ACFREQ.df[ACFREQ.df==Inf] = 'Inf'
+  
+  # Write 
+  write.xlsx(x = ACFREQ.df, file = paste0(settings$Output$Zygosity, 'HLA_Zygosity','.xlsx', sep = ''), sheetName = locus,
+             col.names = TRUE, row.names = FALSE, append = TRUE)
+}
+
