b30e263b2adacf6de5d5cac8accad13dee2563e9
vicentepese <sempereperisvicente@gmail.com>
2021-03-02 08:56:06 -0800

    init

---
+++ HLA_zygosity.R
@@ -0,0 +1,65 @@
+## ---------------------------
+##
+## Name: HLA_zygosity.R
+##
+## Desciption: Given a dataset of HLA calls it performs 
+##              homo/heterozygosity study of a provided allele.
+##
+## Author: Vicente Peris Sempere
+##
+## Year: 2021
+##
+## Copyright (c) Vicente Peris Sempere, 2021
+## Email: vipese@stanford.edu
+##
+## ---------------------------
+
+# Import libraries
+library(jsonlite)
+library(tidyverse)
+library(readr)
+library(data.table)
+library(xlsx)
+library(plyr)
+library(epitools)
+library(haplo.stats)
+
+############## INITIALIZATION ############## 
+
+# Set working directory
+setwd("~/Documents/HLA_association_pipeline")
+
+# Import settings
+settings <- jsonlite::read_json("settings.json")
+
+# Create comand
+`%notin%` <- Negate(`%in%`)
+
+# Import HLA calls, covariates 
+HLA.df <- read.csv(settings$file$HLA_Data)
+covars.df <- read.csv(settings$file$covars)
+probs.df <- read.csv(settings$file$probs)
+
+# Correct pheno for logistic regression 
+if (2 %in% covars.df$pheno %>% table() %>% names()){
+  covars.df$pheno <- covars.df$pheno - 1
+}
+
+# Read options
+prob_thr <- settings$prob_thr
+freq_thr <- settings$freq_thr*100
+
+# Parse HLA calls for which there is a phenotype 
+HLA.df <- HLA.df %>% filter(sample.id %in% covars.df$sample.id)
+
+# Parse HLA calls based on ethnicity, if provided
+if (!settings$ethnicity %>% is_empty()){
+  
+  # Parse IDs in ethnicity/ies
+  ethnicity.df <- read.csv(settings$file$ethnicity)
+  ethnicity.df.filt <- ethnicity.df %>% filter(Population %in% settings$ethnicity %>% unlist())
+  HLA.df <- HLA.df %>% 
+    filter(sample.id %in% ethnicity.df.filt$sample.id)
+}
+
+############## ZYGOSITY ANALYSIS ############## 
