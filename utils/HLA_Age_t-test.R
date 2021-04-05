d1c9b4bb93370386e76ea3e780abce6d681c3bcd
vicentepese <sempereperisvicente@gmail.com>
2021-04-05 09:52:14 -0700

    include dr7dr4 comb

--- utils/HLA_Age_t-test.R
+++ utils/HLA_Age_t-test.R
@@ -87,7 +87,7 @@ HLA.G4 <- HLA.hetero %>% filter(DRB1.1 != "04:02" & DRB1.2 != "04:02")
 HLA.G5<- HLA.neg %>% filter(DRB1.1 != "04:02" & DRB1.2 != "04:02")
 
 # # Compute t-test 
-# hetero.test <- t.test(HLA.hetero$Age[!is.na(HLA.hetero$Age)], HLA.ref$Age[!is.na(HLA.ref$Age)])
+# hetero.test <- t.test(HLA.hetero$Age[!is.na(HLA.hetero$Age)], HLA.neg$Age[!is.na(HLA.neg$Age)])
 # homo.test <- t.test(HLA.hetero$Age[!is.na(HLA.hetero$Age)], HLA.homo$Age[!is.na(HLA.homo$Age)])
 
 # Compute t-test
@@ -108,16 +108,15 @@ age.df <- data.frame(allele1 = c("DR7-", "DR7+", "DR7-","DR7+","DR7+"), allele2=
                      p.val= c(G3.t$p.value, G2.t$p.value, G5.t$p.value, G1.t$p.value, "Ref")
                      )
 
-# age.df <- data.frame(allele1 = c("DR7-", "DR7+", "DR7-","DR7+","DR7+"), allele2= c("DR4+", "DR4+","DR4-","DR7+","DR4-"),
-#                      Ncases = (nrow(HLA.G3), nrow(HLA.G2), )
-#                      mean.Age = c(mean(HLA.homo$Age, na.rm = T), mean(HLA.hetero$Age, na.rm = T), mean(HLA.ref$Age, na.rm = T)),
-#                      std.Age = c(sd(HLA.homo$Age, na.rm = T), sd(HLA.hetero$Age, na.rm = T), sd(HLA.ref$Age, na.rm = T)),
-#                      T.value.HOMO = c(homo.test$statistic, "Ref", NA), 
-#                      T.LI.HOMO = c(homo.test$conf.int[1], "Ref", NA), T.UI.HOMO = c(homo.test$conf.int[2], "Ref", NA),
-#                      T.PVAL.HOMO = c(homo.test$p.value, "Ref", NA),
-#                      T.value.HET = c(NA, hetero.test$statistic, "Ref"), 
-#                      T.LI.HET = c(NA, hetero.test$conf.int[1], "Ref"), T.UI.HET = c(NA, hetero.test$conf.int[2], "Ref"),
-#                      T.PVAL.HOMO = c(NA, hetero.test$p.value, "Ref"))
+age.df <- data.frame(allele1 = c(allele, allele, "X"), allele2= c(allele, "X", "X"),
+                     mean.Age = c(mean(HLA.homo$Age, na.rm = T), mean(HLA.hetero$Age, na.rm = T), mean(HLA.neg$Age, na.rm = T)),
+                     std.Age = c(sd(HLA.homo$Age, na.rm = T), sd(HLA.hetero$Age, na.rm = T), sd(HLA.neg$Age, na.rm = T)),
+                     T.value.HOMO = c(homo.test$statistic, "Ref", NA),
+                     T.LI.HOMO = c(homo.test$conf.int[1], "Ref", NA), T.UI.HOMO = c(homo.test$conf.int[2], "Ref", NA),
+                     T.PVAL.HOMO = c(homo.test$p.value, "Ref", NA),
+                     T.value.HET = c(NA, hetero.test$statistic, "Ref"),
+                     T.LI.HET = c(NA, hetero.test$conf.int[1], "Ref"), T.UI.HET = c(NA, hetero.test$conf.int[2], "Ref"),
+                     T.PVAL.HOMO = c(NA, hetero.test$p.value, "Ref"))
 
 # Write 
 write.xlsx(x = age.df, file = "Outputs/Utils/Age_T_Test.xlsx", sheet = "Sheet1")
